09537e03a783d0c0f39ca9ddc62f0c06415ab8d4
hiwad rashad <hiwadrashad112@hotmail.com>
2021-07-08 14:49:02 +0200

    Added logging via SeriLogger

--- OVRoutePlanner/Program.cs
+++ OVRoutePlanner/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Shared.Logging.SeriLogger;
 
 namespace OVRoutePlanner
 {
@@ -13,11 +15,12 @@ namespace OVRoutePlanner
     {
         public static void Main(string[] args)
         {
+            Setup.Startup();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).ConfigureLogging(logging => { logging.AddSerilog(); })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
---
+++ Shared/Logging/SeriLogger/Setup.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Logging.SeriLogger
+{
+    public class Setup
+    {
+        public static void Startup()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File("..\\Shared\\Logging\\Logs\\log.txt").CreateLogger();
+        }
+    }
+}
